b5416cb0b6493d22453f61b4a45000db601d5ca4
chenchangjian <chenchangjian@gmail.com>
2016-04-19 21:53:31 +0800

    temp

--- eastict-parent/eastict-service/src/main/java/com/eastict/service/pam/user/UserService.java
+++ eastict-parent/eastict-service/src/main/java/com/eastict/service/pam/user/UserService.java
@@ -1,5 +1,7 @@
 package com.eastict.service.pam.user;
 
+import eastict.pojo.pam.Tuser;
+
 public interface UserService {
-	
+	//Tuser createUser(String userid,String logonname,String displayname,);
 }
