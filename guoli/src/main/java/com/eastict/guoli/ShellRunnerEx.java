ec9fea026c95bf825ca0748c493fbd611d60e8a4
chenchangjian <chenchangjian@gmail.com>
2016-05-02 12:39:40 +0800

    temp

--- guoli/src/main/java/com/eastict/guoli/ShellRunnerEx.java
+++ guoli/src/main/java/com/eastict/guoli/ShellRunnerEx.java
@@ -3,10 +3,15 @@ package com.eastict.guoli;
 //支持从classpath获取MyBatis-Generator配置文件
 import static org.mybatis.generator.internal.util.messages.Messages.getString;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -22,6 +27,8 @@ import org.mybatis.generator.api.ProgressCallback;
 import org.mybatis.generator.api.VerboseProgressCallback;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.Context;
+import org.mybatis.generator.config.JDBCConnectionConfiguration;
+import org.mybatis.generator.config.SqlMapGeneratorConfiguration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.exception.XMLParserException;
@@ -47,11 +54,10 @@ public class ShellRunnerEx {
 	public static void main(String[] args) {
 		ArrayList<String> lstPojoNamespace = new ArrayList<String>();
 		ArrayList<String> lstMapperNamespace = new ArrayList<String>();
-/*		if (args.length == 0) {
-			usage();
-			// System.exit(0);//别退出去啊！
-			return; // only to satisfy compiler, never returns
-		}*/
+		/*
+		 * if (args.length == 0) { usage(); // System.exit(0);//别退出去啊！ return;
+		 * // only to satisfy compiler, never returns }
+		 */
 
 		Map<String, String> arguments = new HashMap<String, String>();
 		arguments.put(CONFIG_FILE, "generatorConfig.xml");
@@ -132,7 +138,7 @@ public class ShellRunnerEx {
 				Enumeration<URL> dirs = ShellRunnerEx.class.getClassLoader().getResources(tmpDir);
 				if (dirs.hasMoreElements()) {
 					// 目标命名空间已经存在
-					//URL url = dirs.nextElement();
+					// URL url = dirs.nextElement();
 					mapperExist = true;
 					break;
 				}
@@ -149,37 +155,80 @@ public class ShellRunnerEx {
 				System.out.println("果粒代码生成器-----by 北京东方信通科技有限公司 陈昌建");
 				System.out.println("检测到POJO,Mapper,开始成Service,Controller,JSP的内容");
 				System.out.println("如要重新生成POJO,Mapper,请删除项目中的对应包后再次运行本程序");
+				// 加载指定样式的模板！
+				StringBuilder sbSpringDaoXml = new StringBuilder();
+				// 初始化模板
+				HashMap<String, String> mapPattern = new HashMap<String, String>();
+				mapPattern.clear();
+				String jspStyle = "stylebase";
+				String path = "tmplates/" + jspStyle + "/";
+				String[] types = new String[] { "datasource", "sessionfactory", "mapperscanner", "daoxml" };
+				String fileName = "";
+				String exp = "";
+				for (String typ : types) {
+					fileName = path + typ + ".gl";
+					exp = file2String(fileName);
+					mapPattern.put(typ, exp);
+				}
 				for (Context ctx : ctxs) {
 					String targetPojoPackage = ctx.getJavaModelGeneratorConfiguration().getTargetPackage();
-					String targetMapperPackage = ctx.getSqlMapGeneratorConfiguration().getTargetPackage();
-					//生成Service
-					//Shell.class.getClassLoader().
-					//生成Controller
-					
-					//生成JSP
-					//jsp模板样式(模板组)
-					String jspStyle = "stylebase";
+					SqlMapGeneratorConfiguration sqlMapGeneratorConfiguration = ctx.getSqlMapGeneratorConfiguration();
+					String targetMapperPackage = sqlMapGeneratorConfiguration.getTargetPackage();
+					// 生成Service
+					// Shell.class.getClassLoader().
+					// 生成Controller
+
+					// 生成JSP
+					// jsp模板样式(模板组)
 					String savePath = ctx.getJavaModelGeneratorConfiguration().getTargetProject();
-					//被扫描的命名空间
+					// 被扫描的命名空间
 					String pojoPackage = targetPojoPackage;
 					String mapperPackage = targetMapperPackage;
-					//生成文件的命名空间
-					String targetServicePackage =  "eastict.service";
-					String targetControllerPackage =  "eastict.controller";
-					if(targetMapperPackage.indexOf(".mapper.") > 0){
+					// 生成文件的命名空间
+					String targetServicePackage = "eastict.service";
+					String targetControllerPackage = "eastict.controller";
+					if (targetMapperPackage.indexOf(".mapper.") > 0) {
 						targetServicePackage = targetMapperPackage.replaceAll("mapper", "service");
 						targetControllerPackage = targetMapperPackage.replaceAll("mapper", "controller");
 					}
 					ServiceGenerator gen = new ServiceGenerator();
-					//扫描eastict.mapper.pam包下的所有Mapper,生成Service到./genSource目录下面，包名为eastict.service
-					gen.genServices(mapperPackage,targetServicePackage,savePath);
-					//扫描eastict.pojo.pam包下的所有pojo,生成jsp到./genSource目录下面，目录名为:jsp
+					// 扫描eastict.mapper.pam包下的所有Mapper,生成Service到./genSource目录下面，包名为eastict.service
+					gen.genServices(mapperPackage, targetServicePackage, savePath);
+					// 扫描eastict.pojo.pam包下的所有pojo,生成jsp到./genSource目录下面，目录名为:jsp
 					JspGenerator jspgen = new JspGenerator();
-					jspgen.genJsps(jspStyle, pojoPackage,"./genJSP");
-					//生成Controller
+					jspgen.genJsps(jspStyle, pojoPackage, "./genJSP");
+					// 生成Controller
 					ControllerGenerator conGen = new ControllerGenerator();
-					conGen.genControllers(pojoPackage, mapperPackage, targetServicePackage, targetControllerPackage, savePath);
+					conGen.genControllers(pojoPackage, mapperPackage, targetServicePackage, targetControllerPackage,
+							savePath);
+					// 生成Spring数据源配置
+					
+					String ctxid = ctx.getId();
+					String first = ctxid.substring(0, 1);
+					String end = ctxid.substring(1);
+					ctxid = first.toLowerCase() + end;
+					String dataSourceId = ctxid + "DataSource";
+					String sessionFactoryId = ctxid + "SessionFactory";
+					JDBCConnectionConfiguration jdbcConnectionConfiguration = ctx.getJdbcConnectionConfiguration();
+					String connectionURL = jdbcConnectionConfiguration.getConnectionURL();
+					String driverClass = jdbcConnectionConfiguration.getDriverClass();
+					String userId = jdbcConnectionConfiguration.getUserId();
+					String password = jdbcConnectionConfiguration.getPassword();
+					String datasourceStr = String.format(mapPattern.get("datasource"), dataSourceId, connectionURL,
+							userId, password, driverClass);// 数据源字符串
+					// 生成SqlSessionFactory
+					String sessionFactoryStr = String.format(mapPattern.get("sessionfactory"), sessionFactoryId,
+							dataSourceId);// SessionFactory字符串
+					// 生成MapperScanner配置
+					String mpscannerStr = String.format(mapPattern.get("mapperscanner"), mapperPackage,
+							sessionFactoryId);// SessionFactory字符串
+					sbSpringDaoXml.append(datasourceStr);
+					sbSpringDaoXml.append(sessionFactoryStr);
+					sbSpringDaoXml.append(mpscannerStr);
 				}
+				//输出daoXml文件
+				String strDao = String.format(mapPattern.get("daoxml"),sbSpringDaoXml.toString());
+				WriteStringToFile("./genConfig","","applicationContext-dao.xml",strDao);
 			}
 
 		} catch (XMLParserException e) {
@@ -237,6 +286,73 @@ public class ShellRunnerEx {
 		System.out.println();
 	}
 
+	static String file2String(String file) {
+		// ClassLoader loader = this.getClass().getClassLoader();
+		InputStream stream = ShellRunnerEx.class.getResourceAsStream(file);
+		InputStreamReader sreader = new InputStreamReader(stream, Charset.defaultCharset());
+		BufferedReader reader = new BufferedReader(sreader);
+		String tmpStr = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			while ((tmpStr = reader.readLine()) != null) {
+				sb.append(tmpStr);
+				sb.append("\n");
+			}
+		} catch (IOException ex) {
+
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return sb.toString();
+
+	}
+	static void WriteStringToFile(String basePath, String nameSpace, String fileName, String content) {
+		String tmp = nameSpace.replace(".", "/");
+		String fullPath = basePath + "/" + tmp;
+		fullPath = fullPath.replace("//", "/");
+		ensurePath(fullPath);
+		String newFile = fullPath + "/" + fileName;
+		File txt = new File(newFile);
+		try {
+			if (!txt.exists()) {
+				txt.createNewFile();
+			}
+			FileWriter writer = new FileWriter(txt);
+			BufferedWriter bwriter = new BufferedWriter(writer);
+			bwriter.write(content);
+			//writer.close();
+			bwriter.flush();
+			bwriter.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	static void ensurePath(String fullPath) {
+		String paths[] = fullPath.split("/");
+		String dir = paths[0];
+		File dirFile = new File(dir);
+		if (!dirFile.exists()) {
+			dirFile.mkdir();
+			System.out.println("创建目录为：" + dir);
+		}
+		for (int i = 1; i < paths.length; i++) {
+			try {
+				dir = dir + "/" + paths[i];
+				dirFile = new File(dir);
+				if (!dirFile.exists()) {
+					dirFile.mkdir();
+					System.out.println("创建目录为：" + dir);
+				}
+			} catch (Exception err) {
+				System.err.println("文件夹创建发生异常");
+			}
+		}
+	}
 	private static Map<String, String> parseCommandLine(String[] args) {
 		List<String> errors = new ArrayList<String>();
 		Map<String, String> arguments = new HashMap<String, String>();
